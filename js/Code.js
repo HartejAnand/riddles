f1342712bb1a67dd01ffc39e4e2b2af770e7825d
HartejAnand <76600578+HartejAnand@users.noreply.github.com>
2021-04-19 14:50:45 -0400

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,22 @@
+const accessCode1 = "VARIABLE";
+const accessCode3 = "FUNCTION";
+const accessCode2 = "DATABASE";
+
+function clues() {
+    
+    fill(255,255,255);
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill(250,150,250);
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill(255,255,255);
+    text("A T A D A S B A",700,175);
+    fill(250,150,250);
+    text("Hint: Where values are stored online",700,195);
+    fill(255,255,255);
+    text("C U T N I F O N",200,250);
+    fill(250,150,250);
+    text("Hint: f(x) in math is also the ___ of x",200,270);
+
+}
